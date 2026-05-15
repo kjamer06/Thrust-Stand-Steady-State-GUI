eba58523d66669a7b03901d22ac6dd45f391448b
Keller McGriff <keller.mcgriff@proton.me>
2026-05-15 15:35:31 -0500

    Improved GUI with a pop up for drone specifications. Need to implement landing simulation because it is not working as intended. Need to control the rate it descends.

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-# Mission simulation script for TYTO Series 1585 Thrust Stand
-# User will be able to configure a sequence of events to mimic a given mission profile which will be implemented
-# with a GUI interface. The user will be able to simulate events such as takeoff, hovering at target power, cruise at
-# target trim, etc.
+"""
+Mission simulation script for TYTO Series 1585 Thrust Stand
+ User will be able to configure a sequence of events to mimic a given mission profile which will be implemented
+ with a GUI interface. The user will be able to simulate events such as takeoff, hovering at target power, cruise at
+ target trim, etc.
 
-# REMINDER: THIS WAS BUILT AND TESTED WITH SIMULATED BOARD IN TYTO FLIGHT STAND SOFTWARE
-# ACTUAL THRUST STAND WILL BE MUCH DIFFERENT SO CHANGE VALUES WHEN STAND GETS HERE
+ REMINDER: THIS WAS BUILT AND TESTED WITH SIMULATED BOARD IN TYTO FLIGHT STAND SOFTWARE
+ ACTUAL THRUST STAND WILL BE MUCH DIFFERENT SO CHANGE VALUES WHEN STAND GETS HERE
 
+ IMPORTANT: TYTO Flight Stand software is required to run this program, the link to which can be found below:
+ https://www.tytorobotics.com/blogs/manuals-and-datasheets/software-download
+
+"""
 
 import time
 import dearpygui.dearpygui as dpg
 import matplotlib as mpl
-from dearpygui.dearpygui import child_window
 from matplotlib.image import thumbnail
 from flightstand import FlightStand
 from Button import *
 
-#Constants (Change these as needed)
+#Constants (they will be assigned once in the drone specifications window so they kinda are constants but not really)
 THROTTLE_MAX = 2000
 THROTTLE_MIN = 1000
-THRUST_MAX = 1.8  # This is for simulation thrust stand, actual thrust stand will be 5 kgf
+THRUST_MAX = 1.8
 POWER_MAX = 47
+THRUST_WEIGHT_RATIO = None
 
 # Flightstand initialization
 flightstand = FlightStand()
@@ -34,7 +39,6 @@ voltage_sensor = flightstand.find_input_by_type(flightstand.Proto.VOLTAGE_HV_INP
 current_sensor = flightstand.find_input_by_type(flightstand.Proto.CURRENT_HALL_CURRENT)
 
 event_sequence = []
-
 
 # DPG Functions
 def sequence_builder(sender):
@@ -55,6 +59,16 @@ def sequence_builder(sender):
         case "clear_button":
             event_sequence.clear()
 
+def drone_specifications_callback():
+    global THRUST_MAX, POWER_MAX, MASS, THRUST_WEIGHT_RATIO
+    THRUST_MAX = dpg.get_value("max_thrust_input")
+    POWER_MAX = dpg.get_value("max_power_input")
+    MASS = dpg.get_value("mass_input")
+    THRUST_WEIGHT_RATIO = THRUST_MAX / MASS
+
+    # Close drone specifications window and add info to log console
+    dpg.configure_item("Drone Specifications", show=False)
+    dpg.add_text("[INFO] Drone specifications submitted successfully", parent="console_section")
 
 # Simulation functions
 def spin_propellers():
@@ -64,36 +78,39 @@ def spin_propellers():
     while time.monotonic() < spin_time:
         rotation_value = flightstand.get_latest_input_sample(rotation_speed_sensor).filtered_value
         rotation_error = (500 - rotation_value) / 1000
-        #########################################################################
+
         if throttle.output_target.target_value + rotation_error > THROTTLE_MAX or throttle.output_target.target_value + rotation_error < THROTTLE_MIN:
             throttle.output_target.target_value -= rotation_error
         else:
             throttle.output_target.target_value += rotation_error
-        #########################################################################
         flightstand.update_output(throttle, ['output_target'])
 
+"""
+Things to improve:
+ 1. Bounds errors and input handling (allegedly done, test next time you are here)
+ 2. Go to target thrust is good, just research setting the height if needed.
+ 3. Implement error handling
+ 4. Research better control systems for target thrust if needed
+"""
 
-# Things to improve:
-# 1. Bounds errors and input handling (allegedly done, test next time you are here)
-# 2. Go to target thrust is good, just research setting the height if needed.
-# 3. Implement error handling
-# 4. Research better control systems for target thrust if needed
 def takeoff_simulation(target_thrust):
-    spin_propellers()
-    dpg.add_text("[SIM] Starting takeoff simulation", parent="console_section")
     flightstand.update_output(throttle, ['output_target'])
-    takeoff_time = time.monotonic() + 10
-    while time.monotonic() < takeoff_time:
-        measured_thrust = flightstand.get_latest_input_sample(thrust_sensor).filtered_value
-        thrust_error = target_thrust - measured_thrust
-        #########################################################################
-        if throttle.output_target.target_value + thrust_error > THROTTLE_MAX or throttle.output_target.target_value + thrust_error < THROTTLE_MIN:
-            throttle.output_target.target_value -= thrust_error
-        else:
-            throttle.output_target.target_value += thrust_error
-        #########################################################################
-        flightstand.update_output(throttle, ['output_target'])
+    if target_thrust <= MASS * 9.81:
+        dpg.add_text("[ERR] Target thrust is too low to takeoff", parent="console_section")
+    else:
+        takeoff_time = time.monotonic() + 10
+        spin_propellers()
+        dpg.add_text("[SIM] Starting takeoff simulation", parent="console_section")
+        while time.monotonic() < takeoff_time:
+            measured_thrust = flightstand.get_latest_input_sample(thrust_sensor).filtered_value
+            thrust_error = target_thrust - measured_thrust
 
+            if throttle.output_target.target_value + thrust_error > THROTTLE_MAX or throttle.output_target.target_value + thrust_error < THROTTLE_MIN:
+                throttle.output_target.target_value -= thrust_error
+            else:
+                throttle.output_target.target_value += thrust_error
+            #########################################################################
+            flightstand.update_output(throttle, ['output_target'])
 
 def hover_simulation(target_power):
     dpg.add_text("[SIM] Starting hover simulation", parent="console_section")
@@ -110,14 +127,22 @@ def hover_simulation(target_power):
 
         flightstand.update_output(throttle, ['output_target'])
 
-
+"""
+Things to do for cruise and landing simulation:
+1. Cruise simulation will take in a velocity, and distance and calculate the time needed to complete the cruise segment
+2. Cruise simulation will then adjust the throttle to maintain target velocity for the duration of the cruise segment
+3. Landing simulation will take in a descent rate and calculate the time needed to complete the landing segment.
+4. Landing simulation will then decrease the throttle at a rate that achieves the target descent rate and maintain that descent rate for the duration of the landing segment.
+"""
 def cruise_simulation():
     dpg.add_text("[SIM] Starting cruise simulation", parent="console_section")
 
 
-
 def landing_simulation():
     dpg.add_text("[SIM] Starting landing simulation", parent="console_section")
+    while throttle.output_target.target_value > THROTTLE_MIN:
+        throttle.output_target.target_value -= 0.1
+        flightstand.update_output(throttle, ['output_target'])
 
 
 def simulation_loop():
@@ -140,7 +165,7 @@ def simulation_loop():
     throttle.output_target.target_value = 1000
     flightstand.update_output(throttle, ['output_target'])
 
-
+# Main function handles the GUI frontend and general program initialization.
 def main():
     board = flightstand.create_simulated_board()
     dpg.create_context()
@@ -157,9 +182,7 @@ def main():
             no_collapse=True,
             no_close=True
     ):
-        # =========================
-        # INPUT SECTION
-        # =========================
+        
         with dpg.child_window(
                 tag="input_section",
                 pos=(0, 0),
@@ -199,9 +222,7 @@ def main():
                 format="%.2f"
             )
 
-        # =========================
-        # CONSOLE SECTION
-        # =========================
+        # The console section will display the status of the simulation and any error that might occur throughout the mission sequence
         with dpg.child_window(
                 tag="console_section",
                 pos=(0, 195),
@@ -211,9 +232,7 @@ def main():
         ):
             dpg.add_text("Console")
 
-        # =========================
-        # MISSION BUILDER SECTION
-        # =========================
+        # The events section will display the buttons to add events to the sequence as well as the current sequence of events. The mission sequence will be updated in real time as the user adds and removes events from the sequence.
         with dpg.child_window(
                 tag="events_section",
                 pos=(1005, 195),
@@ -291,8 +310,40 @@ def main():
             ):
                 dpg.add_text("Mission Sequence")
                 mission_display = dpg.add_text("")
+    with dpg.window(
+                tag="Drone Specifications",
+                modal=True,
+                height=200,
+                width=350,
+                label="Drone Specifications"
+        ):
+            dpg.add_text("Input drone specifications here")
+            dpg.add_input_float(
+                label="Max Thrust (N)",
+                tag="max_thrust_input",
+                default_value=THRUST_MAX,
+                format="%.2f"
+            )
+            dpg.add_input_float(
+                label="Max Power (W)",
+                tag="max_power_input",
+                default_value=POWER_MAX,
+                format="%.2f"
+            )
+            dpg.add_input_float(
+                label="Mass (kg)",
+                tag="mass_input",
+                default_value=0,
+                format="%.2f"
+            )
 
-    # Viewport
+            dpg.add_button(
+                label="Submit",
+                callback= drone_specifications_callback,
+                tag="submit_specs_button"
+            )
+
+    # Initialize the GUI window and start the application 
     dpg.create_viewport(
         title='TYTO Series 1585 Thrust Stand Control Script',
         width=1310,
@@ -301,8 +352,6 @@ def main():
     )
 
     dpg.setup_dearpygui()
-
-    # Make the single window the primary window
     dpg.set_primary_window("main_window", True)
 
     dpg.show_viewport()
